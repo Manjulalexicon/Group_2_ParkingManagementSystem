e4ea6477e06495641c68ef19464b6b6ab0987e4a
Maja <maja.bloom@outlook.com>
2026-04-09 14:21:59 +0200

    Implement Vehicle class with plateNumber and getter

--- src/main/java/com/parkingapp/model/Vehicle.java
+++ src/main/java/com/parkingapp/model/Vehicle.java
@@ -3,10 +3,21 @@ package model;
 public class Vehicle {
 
     //-plateNumber:String
+    private final String plateNumber;
 
     // Constructor for adding Vehicle
+    public Vehicle(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
 
     // Getter - to read platenumbers
+    public String getPlateNumber() {
+        return plateNumber;
+    }
 
     // toString
+    @Override
+    public String toString() {
+        return "Vehicle{" + "plateNumber='" + plateNumber + '\'' + '}';
+    }
 }
